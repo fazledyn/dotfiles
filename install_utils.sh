b3a834334920b6c82a74572ca26c8817db3b09e3
fazledyn-or <ataf@openrefactory.com>
2024-07-29 14:33:49 +0600

    Added `install_utils.sh` for easy install

---
+++ install_utils.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# tools
+sudo apt install -y wget curl tmux screen jq tree cloc
+
+# build
+sudo apt install make build-essential g++ gcc
+
+# network
+sudo apt install -y nginx ufw net-tools nmap
+
+# python3
+sudo apt install -y python3-virtualenv python3-pip
