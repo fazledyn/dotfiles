77d8e97496768074faac96bf219964b51f9eecf5
fazledyn <rabidahamed@gmail.com>
2024-08-25 05:02:02 +0600

    Updated Neovim Config

--- nvim.init.lua
+++ nvim.init.lua
@@ -7,7 +7,8 @@ vim.cmd("set shiftwidth=4")
 
 
 -- Bootstrap lazy.vim
-local lazypath = "/home/ataf/.config/nvim/lazy/lazy.nvim"
+-- local lazypath = "/home/ataf/.config/nvim/lazy/lazy.nvim"
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
   local lazyrepo = "https://github.com/folke/lazy.nvim"
