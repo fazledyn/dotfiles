44e485bec5c26b89bd980dd80430c41aef33353c
fazledyn-or <ataf@openrefactory.com>
2024-09-19 11:43:27 +0600

    Updated .bash_path based on TMUX

--- install_utils.sh
+++ install_utils.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 # tools
-sudo apt install -y wget curl tmux screen jq tree cloc
+sudo apt install -y wget curl tmux screen jq tree cloc zsh
 
 # build
 sudo apt install make build-essential g++ gcc
 
 # network
-sudo apt install -y nginx ufw net-tools nmap
+sudo apt install -y nginx ufw net-tools nmap dnsutils
 
 # python3
 sudo apt install -y python3-virtualenv python3-pip
